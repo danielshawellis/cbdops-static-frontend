d6250054f02e9cbd96a0e23cdef26c80083b8653
Daniel Ellis <danielshawellis@gmail.com>
2020-07-19 16:57:21 -0500

    Separate CSS loading from JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const extractPlugin = new ExtractTextPlugin({
+  filename: 'bundle.css'
+});
 
 module.exports = {
   entry: './src/index.js',
@@ -40,31 +45,9 @@ module.exports = {
       },
       {
         test: /\.(scss)$/,
-        use: [
-          {
-            // Adds CSS to the DOM by injecting a `<style>` tag
-            loader: 'style-loader'
-          },
-          {
-            // Interprets `@import` and `url()` like `import/require()` and will resolve them
-            loader: 'css-loader'
-          },
-          {
-            // Loader for webpack to process CSS with PostCSS
-            loader: 'postcss-loader',
-            options: {
-              plugins: function () {
-                return [
-                  require('autoprefixer')
-                ];
-              }
-            }
-          },
-          {
-            // Loads a SASS/SCSS file and compiles it to CSS
-            loader: 'sass-loader'
-          }
-        ]
+        use: extractPlugin.extract({
+          use: ['css-loader', 'sass-loader']
+        })
       }
     ]
   },
@@ -77,6 +60,7 @@ module.exports = {
     }
   },
   plugins: [
+    extractPlugin,
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'src/html/index.html'
